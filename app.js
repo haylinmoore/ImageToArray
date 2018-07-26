fb6fe79519a9bdeaf19131a79f1b63547e93e61f
Hampton Moore <me@hampton.pw>
2018-07-26 22:14:21 +0200

    ES6 All the stuffs

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
 // jshint esversion: 6
 // jscs:disable maximumLineLength
 const fs = require('fs');
-let getter = require('pixel-getter');
 
-if (process.argv[2] == undefined){
+const getter = require('pixel-getter');
+
+if (process.argv[2] == undefined) {
   throw "Needs a folder"
 }
 
-var path = process.argv[2];
+const path = process.argv[2];
 
-var checker = null;
-var filecount = null;
-var outputbinary = [];
+let checker = null;
+let filecount = null;
+const outputbinary = [];
 
-var processImage = function (file) {
+const processImage = file => {
   console.log(file)
   getter.get(path + file, (err, pixels) => {
     if (err) {
       throw err;
     }
     let output = [];
-    for (let i = 0; i < pixels[0].length; i += 1) {
 
-      var pixel = pixels[0][i];
-
+    for (const pixel of pixels[0]) {
       //console.log(rgbToBW(pixel))
 
       if (pixel.a == 0) {
@@ -31,15 +30,14 @@ var processImage = function (file) {
       } else {
         output.push(1);
       }
-
     }
 
     outputbinary.push(output);
   });
-}
+};
 
 
-fs.readdir(path, function (err, files) {
+fs.readdir(path, (err, files) => {
   if (err) {
     console.error("Could not list the directory.", err);
     process.exit(1);
@@ -47,21 +45,21 @@ fs.readdir(path, function (err, files) {
 
   filecount = 0;
 
-  for (var i in files) {
+  for (const i in files) {
     if (files[i].includes("png")) {
       filecount++;
-      console.log("Processing: " + files[i]);
+      console.log(`Processing: ${files[i]}`);
       processImage(files[i]);
     }
   }
 
-  console.log("Total File Count Is " + filecount)
+  console.log(`Total File Count Is ${filecount}`)
 
 
-  checker = setInterval(function () {
+  checker = setInterval(() => {
     if (outputbinary.length == filecount) {
-      console.log("Finished, output has been written to " + path + "output.json");
-      fs.writeFileSync(path + "output.json", JSON.stringify(outputbinary), 'utf8');
+      console.log(`Finished, output has been written to ${path}output.json`);
+      fs.writeFileSync(`${path}output.json`, JSON.stringify(outputbinary), 'utf8');
       clearInterval(checker);
     }
   }, 1)
